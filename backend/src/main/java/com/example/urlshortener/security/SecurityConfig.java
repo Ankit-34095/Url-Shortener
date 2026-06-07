37cd0b5c8ad7f83f3062f024466c5b2811769d91
Ankit-34095 <132091395+Ankit-34095@users.noreply.github.com>
2026-06-07 07:49:02 +0530

    Updated SecurityConfig.java

--- backend/src/main/java/com/example/urlshortener/security/SecurityConfig.java
+++ backend/src/main/java/com/example/urlshortener/security/SecurityConfig.java
@@ -69,7 +69,10 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of(
+        "http://localhost:3000",
+        "https://*.vercel.app"
+));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
