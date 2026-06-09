5caf03355426cb2dbf2e0679b58c140ef1e8ada7
Ankit-34095 <132091395+Ankit-34095@users.noreply.github.com>
2026-06-09 10:55:15 +0530

    Updated GeoLocationService.java
    Injected Rest Template

--- backend/src/main/java/com/example/urlshortener/service/GeoLocationService.java
+++ backend/src/main/java/com/example/urlshortener/service/GeoLocationService.java
@@ -4,6 +4,9 @@ import com.example.urlshortener.model.Click;
 
 public interface GeoLocationService {
     // Interface for geolocation services to enrich click data
+    @Autowired
+    private RestTemplate restTemplate;
+    
     void enrichClickWithLocation(Click click);
 }
 
