3f1be643ac03572ef807f3363d686f13003b089f
Ankit-34095 <132091395+Ankit-34095@users.noreply.github.com>
2026-06-09 10:52:10 +0530

    Created IpApiResponse.java

---
+++ backend/src/main/java/com/example/urlshortener/service/IpApiResponse.java
@@ -0,0 +1,11 @@
+package com.example.urlshortener.service.dto;
+
+import lombok.Data;
+
+@Data
+public class IpApiResponse {
+
+    private String country;
+    private String country_code;
+    private String city;
+}
